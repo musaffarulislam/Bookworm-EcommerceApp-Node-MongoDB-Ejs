c2290991b98b639f090ba835ae766e7e0c7210db
musaffarulislam <musaffarulislam@gmail.com>
2023-02-04 02:28:27 +0530

    Edit Book done - dropdown correct it

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const sessions = require('express-session');
 const logger = require('morgan');
@@ -44,8 +43,6 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
