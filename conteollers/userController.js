917c90be9782cbbafe466b872c414da5593f90f2
musaffarulislam <musaffarulislam@gmail.com>
2023-02-18 11:42:22 +0530

    signup done not fetch before convrert fetch

--- conteollers/userController.js
+++ conteollers/userController.js
@@ -6,6 +6,8 @@ const crypto = require('crypto');
 const { count } = require('console');
 const Razorpay = require('razorpay'); 
 
+const dotenv = require('dotenv');
+dotenv.config({path : '.env'});
 
 const user = require('../models/userModel');
 const cart = require('../models/cartModel');
@@ -74,80 +76,86 @@ const transporter = nodemailer.createTransport({
     secure: false,
     auth: {
       user: 'bookworm.ecommerce.project@gmail.com',
-      pass: 'vbkxsqzybfuaaylt'
+      pass: 'pcgkxadcvkebxmuo'
     }
 });
    
 
+
 const userSignup = async (req, res) => {
-    try {
-        const existingUser = await user.findOne({ email: req.body.email });
-    
-        if (existingUser) {
-          req.session.errormsg = 'Email already exists';
-          return res.redirect('/signup');
+  try {
+      const existingUser = await user.findOne({ email: req.body.email });
+
+      if (existingUser) {
+        req.session.errormsg = 'Email already exists';
+        return res.redirect('/signup');
+      }
+
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+      const User = {
+          username: req.body.userName,
+          email: req.body.email,
+          phoneNumber: req.body.phoneNumber,
+          age: req.body.age,
+          password: hashedPassword,
+          block: true,
+      };
+
+
+      const OTP = Math.floor(1000 + Math.random() * 9000).toString();
+      const expirationTime = Date.now() + 5 * 60 * 1000;
+
+      console.log(OTP);
+
+      await UserOTPVerification.deleteMany({email: User.email})
+
+      const newOTPVerification = new UserOTPVerification({
+        email: User.email,
+        otp: OTP,
+        expiresAt: expirationTime
+      });
+
+      await newOTPVerification.save();
+
+      const mailOptions = {
+        from: 'bookworm.ecommerce.project@gmail.com',
+        to: User.email,
+        subject: 'OTP',
+        text: `You otp :${OTP}`,
+        html: `
+        <div style="border-bottom:1px solid #eee">
+          <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Bookworm</a>
+        </div>
+        <p style="font-size:1.1em">Hi ${User.username},</p>
+        <p>Thank you for choosing Brandworm. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
+        <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${OTP}</h2>
+        <p style="font-size:0.9em;">Regards,<br />Bookworm</p>
+        <hr style="border:none;border-top:1px solid #eee" />
+        <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+          <p>Bookworm</p>
+        </div>
+        </div>
+        </div>`
+      };
+
+      await transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error(`Error sending email: ${error}`);
         }
-    
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-        const User = {
-            username: req.body.userName,
-            email: req.body.email,
-            phoneNumber: req.body.phoneNumber,
-            age: req.body.age,
-            password: hashedPassword,
-            block: true,
-        };
-
-
-        const OTP = Math.floor(1000 + Math.random() * 9000).toString();
-        const expirationTime = new Date(Date.now() + 5 * 60 * 1000);
-
-        console.log(OTP);
-    
-        await UserOTPVerification.deleteMany({email: User.email})
-
-        const newOTPVerification = new UserOTPVerification({
-          email: User.email,
-          otp: OTP,
-          expiresAt: expirationTime
-        });
-        await newOTPVerification.save();
-
-        const mailOptions = {
-          from: 'bookworm.ecommerce.project@gmail.com',
-          to: User.email,
-          subject: 'OTP',
-          text: `You otp :${OTP}`,
-          html: `
-          <div style="border-bottom:1px solid #eee">
-            <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Bookworm</a>
-          </div>
-          <p style="font-size:1.1em">Hi ${User.username},</p>
-          <p>Thank you for choosing Brandworm. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
-          <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${OTP}</h2>
-          <p style="font-size:0.9em;">Regards,<br />Bookworm</p>
-          <hr style="border:none;border-top:1px solid #eee" />
-          <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-            <p>Bookworm</p>
-          </div>
-          </div>
-          </div>`
-        };
-    
-        await transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            console.error(`Error sending email: ${error}`);
-          }
-          console.log(`OTP sent to ${User.email}: ${OTP}`);
-          res.redirect(`/otp?userName=${User.username}&email=${User.email}&phoneNumber=${User.phoneNumber}&age=${User.age}&password=${User.password}&block=${User.block}&expiresAt=${expirationTime}`);
-        });
-      }catch(err){
-        console.error(`Error User Signup: ${err}`);
-        res.redirect('/signup');
+        console.log(`OTP sent to ${User.email}: ${OTP}`);
+        // res.redirect(`/otp?userName=${User.username}&email=${User.email}&phoneNumber=${User.phoneNumber}&age=${User.age}&password=${User.password}&block=${User.block}`);
+        res.status(200).send({message:"Signup success",
+          userName : User.username,
+          email : User.email,
+          phoneNumber : User.phoneNumber,
+          age : User.age,
+        })
+      });
+    } catch (err) {
+      console.error(`Error inserting user: ${err}`);
     }
 };
-  
 
 const renderOTP = (req,res) => {
     const userInfo = req.query;
