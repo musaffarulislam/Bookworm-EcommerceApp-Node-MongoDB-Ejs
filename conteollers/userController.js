29d375bda2fc9863a62cb2325553a9905984eada
musaffarulislam <musaffarulislam@gmail.com>
2023-02-21 19:54:06 +0530

    razro pay key api change, cashon delivary done coupon address all done, online address coupon not done

--- conteollers/userController.js
+++ conteollers/userController.js
@@ -976,6 +976,26 @@ const cashOnDelivary = async (req, res) => {
   try {
     const userId = req.query.userId;
     const cartItems = await cart.find({ user: userId });
+    let couponName = req.body.couponName;
+    if(couponName){
+      const couponInfo = await coupon.findOne({ couponName });
+
+      const carts = await cart.find({user: userId}).populate('user').populate('product').
+      populate({path: 'product', populate: {path: 'author'}}).populate({path: 'product', populate: {path: 'genre'}})
+      
+      let totalAmount = productTotal(carts)
+      let shipping = false;
+
+      if(!couponInfo){
+        if(totalAmount<400){
+          totalAmount = totalAmount + 40;
+          shipping = true;
+        }
+        return res.status(400).send({message:"Coupon name not valid",totalAmount,shipping})
+      }
+      couponInfo.users.push(req.body.userId);
+      await couponInfo.save();
+    }
 
     const productArray = cartItems.map(item => {
       return { productId: item.product, quantity: item.quantity };
@@ -1011,9 +1031,130 @@ const cashOnDelivary = async (req, res) => {
 };
 
 
+// const onlinePayment = async (req, res) => {
+//   try {
+//     const userId = req.query.userId;
+//     let couponName = req.body.couponName;
+//     if(couponName){
+//       const couponInfo = await coupon.findOne({ couponName });
+
+//       const carts = await cart.find({user: userId}).populate('user').populate('product').
+//       populate({path: 'product', populate: {path: 'author'}}).populate({path: 'product', populate: {path: 'genre'}})
+      
+//       let totalAmount = productTotal(carts)
+//       let shipping = false;
+
+//       if(!couponInfo){
+//         if(totalAmount<400){
+//           totalAmount = totalAmount + 40;
+//           shipping = true;
+//         }
+//         return res.status(400).send({message:"Coupon name not valid",totalAmount,shipping})
+//       }
+
+//     }
+
+
+//     const amount = req.body.totalAmount;
+//     const lastOrder = await order.find().sort({ _id: -1 }).limit(1);
+//     let orderId = 'BKWM000001';
+//     if (lastOrder.length > 0) {
+//       const lastOrderId = lastOrder[0].orderId;
+//       const orderIdNumber = parseInt(lastOrderId.slice(4));
+//       orderId = `BKWM${("000000" + (orderIdNumber + 1)).slice(-6)}`;
+//     }
+
+//     const razorpayInstance = new Razorpay({
+//       key_id: "rzp_test_ifEOe80qfCYvOK",
+//       key_secret: "PIw0PiNYutnX30GerxbnYZNZ"
+//     });
+
+//     let options = await razorpayInstance.orders.create({
+//       amount: amount * 100, 
+//       currency: "INR",
+//       receipt: orderId
+//     });
+
+//     console.log(options)
+
+    
+//     const userDetails = await user.findOne({_id: userId});
+
+//     res.status(201).json({
+//       success: true,
+//       options,
+//       userDetails,
+//       amount,
+//       couponName
+//     });
+//   } catch (err) {
+//     console.error(`Error Online Payment:`, err);
+//     res.status(500).json({
+//       success: false,
+//       error: "Internal server error"
+//     });
+//   }
+// };
+
+
+
+
+// const verifyOnlinePayment = async (req, res) => {
+//   try {
+//     console.log(req.body);
+//     const payment = req.body.payment;
+//     const orderDetails = req.body.order
+//     let hmac = crypto.createHmac('sha256','PIw0PiNYutnX30GerxbnYZNZ')
+//     hmac.update(payment.razorpay_order_id +'|'+ payment.razorpay_payment_id)
+//     hmac=hmac.digest('hex')
+
+//     if(hmac == payment.razorpay_signature){
+//       const userId = req.body.userId;
+//       const cartItems = await cart.find({ user: userId });
+
+//       let productArray = cartItems.map(item => {
+//         return { productId: item.product, quantity: item.quantity };
+//       });
+
+//       let couponName = req.body.couponName;
+//       if(couponName){
+//         const couponInfo = await coupon.findOne({ couponName });
+//         couponInfo.users.push(userId);
+//         await couponInfo.save();
+//       }
+
+//       const newOrder = new order({
+//         orderId : orderDetails.receipt,
+//         user: userId,
+//         product: productArray,
+//         address: req.body.address,
+//         totalAmount: orderDetails.amount/100,
+//         paymentMethod: "Online",
+//       });
+  
+//       await newOrder.save();
+  
+//       await cart.deleteMany({ user: userId });
+  
+//       const orderId = orderDetails.receipt
+//       res.status(200).send({ orderId });
+//     }
+
+//   } catch (err) {
+//     console.error(`Error Verify Online Payment:`, err);
+//     res.status(500).json({
+//       success: false,
+//       error: "Internal server error"
+//     });
+//   }
+// };
+
+
 const onlinePayment = async (req, res) => {
   try {
+    console.log("hiiiiiiiiiiiiiiii  1")
     const amount = req.body.totalAmount;
+    console.log("hiiiiiiiiiiiiiiii  2")
     const lastOrder = await order.find().sort({ _id: -1 }).limit(1);
     let orderId = 'BKWM000001';
     if (lastOrder.length > 0) {
@@ -1022,10 +1163,15 @@ const onlinePayment = async (req, res) => {
       orderId = `BKWM${("000000" + (orderIdNumber + 1)).slice(-6)}`;
     }
 
+    console.log("hiiiiiiiiiiiiiiii  3")
+
     const razorpayInstance = new Razorpay({
-      key_id: "rzp_test_ifEOe80qfCYvOK",
-      key_secret: "PIw0PiNYutnX30GerxbnYZNZ"
+      key_id: "rzp_test_MfMHXbdBnx5BYx",
+      key_secret: "egXXr5eoXtsziGUkWHfUAm0F"
     });
+
+
+    console.log("hiiiiiiiiiiiiiiii  4")
 
     let options = await razorpayInstance.orders.create({
       amount: amount * 100, 
@@ -1033,10 +1179,12 @@ const onlinePayment = async (req, res) => {
       receipt: orderId
     });
 
-    console.log(options)
+    console.log("hiiiiiiiiiiiiiiii  5") 
 
     const userId = req.query.userId;
     const userDetails = await user.findOne({_id: userId});
+
+    console.log("hiiiiiiiiiiiiiiii  6")
 
     res.status(201).json({
       success: true,
@@ -1054,12 +1202,13 @@ const onlinePayment = async (req, res) => {
 };
 
 
+
 const verifyOnlinePayment = async (req, res) => {
   try {
     console.log(req.body);
     const payment = req.body.payment;
     const orderDetails = req.body.order
-    let hmac = crypto.createHmac('sha256','PIw0PiNYutnX30GerxbnYZNZ')
+    let hmac = crypto.createHmac('sha256','egXXr5eoXtsziGUkWHfUAm0F')
     hmac.update(payment.razorpay_order_id +'|'+ payment.razorpay_payment_id)
     hmac=hmac.digest('hex')
 
@@ -1096,8 +1245,6 @@ const verifyOnlinePayment = async (req, res) => {
     });
   }
 };
-
-
 
 
 const logout = (req,res)=>{
