58afcffc5fd414025d7081b12138322462691f3a
musaffarulislam <musaffarulislam@gmail.com>
2023-02-28 19:34:44 +0530

    nodemailer problem 4

--- conteollers/userController.js
+++ conteollers/userController.js
@@ -141,9 +141,9 @@ const userSignup = async (req, res) => {
 
       await transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-          // console.error(`Error sending email: ${error}`);
-          req.session.errormsg = "Email Not Send";
-          res.redirect('/signup')
+          console.error(`Error sending email: ${error}`);
+          // req.session.errormsg = "Email Not Send";
+          // res.redirect('/signup')
         }
         console.log(`OTP sent to ${User.email}: ${OTP}`);
 
