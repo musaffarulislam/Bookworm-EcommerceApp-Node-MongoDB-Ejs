55cfd1f5ed8839d9c66cae36e16c532e39445568
musaffarulislam <musaffarulislam@gmail.com>
2023-09-28 19:07:30 +0530

    server side problem 4

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,18 +23,28 @@ const UserOTPVerification = require('../models/userOTPVerification');
 
 const renderHome = async (req,res)=>{
   try{
+    console.log("render cntrlr ", 1)
     const books = await book.find({delete: {$ne: false}}).populate('author').populate('genre');
+    console.log("render cntrlr ", 2)
     const banners = await banner.findOne({banner: true}).populate('bigCard1ProductId').populate('bigCard2ProductId')
+    console.log("render cntrlr ", 3)
     req.session.userInfo = false
+    console.log("render cntrlr ", 4)
     const userId = req.session.user;
+    console.log("render cntrlr ", 5)
     let userDetails = false;
+    console.log("render cntrlr ", 6)
     if(userId){
       userDetails = await user.findOne({_id: userId})
     }
+    console.log("render cntrlr ", 7)
     const warning = req.session.errormsg;
+    console.log("render cntrlr ", 8)
     req.session.errormsg = false;
+    console.log("render cntrlr ", 9)
     res.render('index',{ title: "Home",books,banners,userDetails,warning});
   }catch(error){
+    console.log("render cntrlr ", 10)
     console.log("render home r=error",error)
   }
 }
