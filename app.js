4466c1c6ef82d998fe64875137a04f450a8bb3bb
musaffarulislam <musaffarulislam@gmail.com>
2023-09-28 20:05:11 +0530

    server side problem 17

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 console.log("Try 4")
 app.use('/admin', adminRouter);
